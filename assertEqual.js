42841d1dfa9fb53d82a0da092bde14604e575103
Noidea22 <mathewsteenwinkel@gmail.com>
2023-02-06 18:20:09 -0700

    added emoji

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log('Assertion Failed: ' + actual + " !== " + expected);
+  } else {
+    console.log('✅');
+  }
+};
+
+//test code
+assertEqual("lighthouse labs", "Bootcamp");
+assertEqual(1,1);
+
