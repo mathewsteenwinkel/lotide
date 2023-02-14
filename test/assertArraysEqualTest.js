6b07f38b186e29444de32bf0f65ede6c05edd7ad
Noidea22 <mathewsteenwinkel@gmail.com>
2023-02-13 19:25:30 -0700

    refractored

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertArraysEqual');
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
